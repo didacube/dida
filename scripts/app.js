046a649f5cda60f0481534e760da720883f92c0e
Ashley Qian <ashley.qian@duke.edu>
2014-10-21 23:13:10 -0400

    moved inline js into seperate app.js file, new styles for other pages

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+$('button#skip').on('click', function() {
+    removeSplash();
+});
+
+// Hide both panes, we'll selectively
+// show which one we want later
+$('#home').hide();
+$('#splash').hide();
+
+// Check for user cookies
+if (Cookie.test()) {
+
+    // Returning User
+    if (Cookie.get('cookies')) {
+        setTimeout(function() {
+            $('#home').fadeIn(1000);
+        }, 400);
+    }
+
+    // Previous User
+    else {
+        Cookie.set('cookies', 'true');
+        $('#splash').fadeIn(1000);
+    }
+}
+
+// Remove the splash dialog and fade
+// in the home page.
+var removeSplash = function() {
+    $('#splash').fadeOut(1000);
+    setTimeout(function() {
+        $('#home').fadeIn(1000);
+    }, 1400);
+};
+
+// Put an active class on the correct
+// link in the navbar
+$('a[href^="' + window.location.pathname + '"]').addClass('active');
