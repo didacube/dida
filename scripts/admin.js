0fc1b6382b5be06aaa2d62f1fd984138d8733e55
Ashley Qian <ashley.qian@duke.edu>
2015-01-13 17:36:40 -0500

    Implemented request form toggle. Shows and hides the request form when an admin toggles it from the dashboard.

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,3 +1,25 @@
+// Get the status of the request form.
+var Display = Parse.Object.extend("Display");
+var query = new Parse.Query(Display);
+var requestForm = null;
+query.equalTo("element", "request");
+query.find({
+	success: function(res) {
+		requestForm = res[0];
+		$(document).ready(function() {
+			$("[name='requestToggle']").bootstrapSwitch({
+				state: requestForm.get("shown")
+			});
+		});
+	},
+	error: function(error) {
+		$(document).ready(function() {
+			// TODO: Implement alerts.
+		});
+	}
+});
+
+// Get the current user, if there is one.
 var currentUser = Parse.User.current();
 if (currentUser) {
 	$(document).ready(function() {
@@ -7,7 +29,7 @@ if (currentUser) {
 }
 
 $(document).ready(function() {
-	$("[name='requestToggle']").bootstrapSwitch();
+	// Sign in the user.
 	$("#admin-sign-in-button").click(function() {
 		var username = $("input[name='username']").val();
 		var password = $("input[name='password']").val();
@@ -16,5 +38,22 @@ $(document).ready(function() {
 			$("#admin-sign-in-form").hide();
 		});
 		return false;
+	});
+
+	// Sign out the user.
+	$("#admin-sign-out-button").click(function() {
+		Parse.User.logOut();
+		$("#admin-dashboard").removeClass("verified");
+		$("#admin-sign-in-form").show();
+	});
+
+	// Toggle the request form to show or hide.
+	$('input[name="requestToggle"]').on('switchChange.bootstrapSwitch', function(event, state) {
+		requestForm.set("shown", state);
+		requestForm.save().then(function(res) {
+			// TODO: Implement alerts.
+		}, function(error) {
+			// TODO: Implement alerts.
+		});
 	});
 });
--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,41 @@
-$('button#skip').on('click', function() {
-    removeSplash();
-});
+$(document).ready(function() {
 
-// Hide both panes, we'll selectively
-// show which one we want later
-$('#home').hide();
-$('#splash').hide();
+    // Skip button transitions to the home page.
+    $('button#skip').on('click', function() {
+        removeSplash();
+    });
 
-// Check for user cookies
-if (Cookie.test()) {
+    // Hide both panes, we'll selectively
+    // show which one we want later.
+    $('#home').hide();
+    $('#splash').hide();
 
-    // Returning User
-    if (Cookie.get('cookies')) {
+    // Check for user cookies.
+    if (Cookie.test()) {
+
+        // Returning User.
+        if (Cookie.get('cookies')) {
+            setTimeout(function() {
+                $('#home').fadeIn(1000);
+            }, 400);
+        } else { // Previous User.
+            Cookie.set('cookies', 'true');
+            $('#splash').fadeIn(1000);
+        }
+    } else {
+        $('#home').fadeIn(1000);
+    }
+
+    // Remove the splash dialog and fade
+    // in the home page.
+    var removeSplash = function() {
+        $('#splash').fadeOut(1000);
         setTimeout(function() {
             $('#home').fadeIn(1000);
-        }, 400);
-    } else { // Previous User
-        Cookie.set('cookies', 'true');
-        $('#splash').fadeIn(1000);
-    }
-} else {
-    $('#home').fadeIn(1000);
-}
+        }, 1400);
+    };
 
-// Remove the splash dialog and fade
-// in the home page.
-var removeSplash = function() {
-    $('#splash').fadeOut(1000);
-    setTimeout(function() {
-        $('#home').fadeIn(1000);
-    }, 1400);
-};
-
-// Put an active class on the correct
-// link in the navbar
-$('a[href^="' + window.location.pathname + '"]').addClass('active');
+    // Put an active class on the correct
+    // link in the navbar.
+    $('a[href^="' + window.location.pathname + '"]').addClass('active');
+});
