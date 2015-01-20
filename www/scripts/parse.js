fc9f2fddf9e699adf435b34bbef4af5996d0abac
Ashley Qian <ashley.qian@duke.edu>
2015-01-20 04:14:30 -0500

    Added .gitignore

--- www/scripts/parse.js
+++ www/scripts/parse.js
@@ -1,5 +1,5 @@
 // For Production
-// Parse.initialize("eWmoSt74v7NojmA0AeQre8lXBbQcAI4oK3UPGs10", "PFhSPlapuKJveidb3fbbOmpAiDgH0QvPP2amUeHV");
+Parse.initialize("eWmoSt74v7NojmA0AeQre8lXBbQcAI4oK3UPGs10", "PFhSPlapuKJveidb3fbbOmpAiDgH0QvPP2amUeHV");
 
 // For Test
-Parse.initialize("8kLrG50e9cA9P7R0Ers0p8qZArPQm3XHnxOn2lpE", "xgt0e9tigrdUzAnJ8KADZgvg0OYYwlXBN3PV0xho");
+// Parse.initialize("8kLrG50e9cA9P7R0Ers0p8qZArPQm3XHnxOn2lpE", "xgt0e9tigrdUzAnJ8KADZgvg0OYYwlXBN3PV0xho");
